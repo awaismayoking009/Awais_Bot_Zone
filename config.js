56165dddf24cf92e17b604b4081c2b2120a1884e
AWAIS MAYO KING009 <rojxb268@gmail.com>
2026-02-02 18:02:12 +0500

    Add configuration file for bot settings
    Initialize configuration settings for the bot.

---
+++ config.js
@@ -0,0 +1,19 @@
+const fs = require('fs');
+
+global.botName = "💀Awais_Mayo_Md👑";
+global.ownerName = "Awais Mayo";
+global.ownerNumber = "923295533214"; 
+global.channelLink = "https://whatsapp.com/channel/0029VbBzlMlIt5rzSeMBE922";
+global.menuImage = "./Awais.png"; 
+
+// Defaults
+global.autoReact = false;
+global.autoStatus = true;
+
+global.mess = {
+    wait: '⚡ _System Processing... Please wait..._',
+    success: '✅ _Task Completed Successfully!_',
+    admin: '🚫 _This command is only for Group Admins!_',
+    owner: '👑 _This command is only for Awais Mayo!_',
+    error: '❌ _Error! Something went wrong._'
+};
